8919e1f1752f855b40d8e3623f88ff797fd1dd6b
bhawnarathor1204 <brathor@sebonetechnologies.com>
2022-05-02 11:16:41 +0530

    Intial commit

---
+++ LMS/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using LMS.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LMS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SignIn signInPage;
+        public SignUp signUpPage;
+        public HomePage homePage;
+        public BookDetails bookDetails;
+        public MainWindow()
+        {
+            InitializeComponent();
+            signInPage=new SignIn();
+            signUpPage=new SignUp();
+            homePage=new HomePage();
+            bookDetails=new BookDetails();
+
+
+        }
+
+        private void SignInPage_Btt_Click(object sender, RoutedEventArgs e)
+        {
+            MainwindowFrame.Content = signInPage;
+        }
+
+        private void SignUpPage_Btt_Click(object sender, RoutedEventArgs e)
+        {
+            MainwindowFrame.Content = signUpPage;
+        }
+
+        private void Back_Bttn(object sender, RoutedEventArgs e)
+        {
+            if (MainwindowFrame.NavigationService.CanGoBack)
+            {
+                MainwindowFrame.NavigationService.GoBack();
+            }
+        }
+
+        private void Forward_Bttn(object sender, RoutedEventArgs e)
+        {
+            if (MainwindowFrame.NavigationService.CanGoForward)
+            {
+                MainwindowFrame.NavigationService.GoForward();
+            }
+        }
+
+        private void HomePage_Btt_Click(object sender, RoutedEventArgs e)
+        {
+            MainwindowFrame.Content= homePage;
+        }
+
+        private void Book_Btt_Click(object sender, RoutedEventArgs e)
+        {
+            MainwindowFrame.Content = bookDetails;
+        }
+    }
+}
---
+++ LMS/Pages/SignIn.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LMS.Pages
+{
+    /// <summary>
+    /// Interaction logic for SignIn.xaml
+    /// </summary>
+    public partial class SignIn : Page
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+        private void go_to_singUp_Page(object sender, RoutedEventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            
+           
+        }
+        private void go_to_home_page(object sender, RoutedEventArgs e)
+        {
+            HomePage homePage = new HomePage();
+           
+
+        }
+    }
+}
